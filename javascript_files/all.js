8e498cbbb7703434eae500c0ac133872b2b9feb4
Yibizo <57368067+Yibizo@users.noreply.github.com>
2020-10-05 13:06:10 -0500

    Add files via upload

---
+++ javascript_files/all.js
@@ -0,0 +1,16 @@
+var menuBlock = '<div>'+
+'<span><a href="about_me.html" class="linkMenu">About Me</a></span>'+
+'<span><a href="skills.html" class="linkMenu">Skills</a></span>'+
+'<span><a href="projects.html" class="linkMenu">Projects</a></span>'+
+'<span><a href="contact.html" class="linkMenu">Contact</a></span>'+
+'</div>'
+
+document.getElementById("menu").innerHTML = menuBlock
+
+var imageBlock = '<div>'+
+'<img src="../images/katana_zero_roof.gif" id="imageMain"/>'+
+'<div><a href="index.html" id="textImage" class="glow">Diego Mejía Suárez</a></div>'+
+'</div>'
+
+document.getElementById("imageHead").innerHTML = imageBlock
+
